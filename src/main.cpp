b3fa3a0a2296cb9ace67c44d2bf21b01c0159476
RangHo Lee <hello@rangho.me>
2021-02-17 00:13:20 +0900

    [wip] Make sure cURL can retrieve The Camp correctly

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,29 @@
+#include <iostream>
+
+#include <curl/curl.h>
+
+#include "your_camp/constants.hpp"
+
 int main(int argc, char* argv[])
 {
+    // Initialize cURL
+    curl_global_init(CURL_GLOBAL_ALL);
+    auto curl = curl_easy_init();
+
+    if (!curl)
+        throw std::exception();
+
+    curl_easy_setopt(curl, CURLOPT_URL, your_camp::THE_CAMP_BASEURL);
+    curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+
+    auto result = curl_easy_perform(curl);
+
+    if (result != CURLE_OK)
+        std::cout << "cURL failed" << std::endl;
+
+    // Cleanup stuff
+    curl_easy_cleanup(curl);
+    curl_global_cleanup();
 
     return 0;
 }
-
